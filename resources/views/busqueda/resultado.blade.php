e9323e12c4aadcf2b740147a3a51741ac97fa383
MiguelSilvaP22 <miguel.silva22@inacapmail.cl>
2018-07-09 23:03:44 -0400

    update

--- resources/views/busqueda/resultado.blade.php
+++ resources/views/busqueda/resultado.blade.php
@@ -19,19 +19,23 @@
         <div class="product_grid_border"></div>
         
         @foreach($repuestos as $repuesto)
-        <!-- Product Item -->
-        <div class="product_item is_new">
-            <div class="product_border"></div>
-            <div class="product_image d-flex flex-column align-items-center justify-content-center"><img src="ecommerce/images/productos/{{$repuesto->imagenrepuesto->last()->ruta_imagenrepuesto}}" alt=""></div>
-            <div class="product_content">
-                <div class="product_price number">${{  number_format($repuesto->precio_repuesto,0) }}</div>
-                <div class="product_name"><div><a href="detallerepuesto/{{$repuesto->id_repuesto}}" tabindex="0">{{$repuesto->nombre_repuesto}}</a></div></div>
+            @if($repuesto->estado_repuesto==1)
+            <!-- Product Item -->
+            <div class="product_item is_new">
+                <div class="product_border"></div>
+                @if($repuesto->imagenrepuesto->count()>0)
+                <div class="product_image d-flex flex-column align-items-center justify-content-center"><img src="ecommerce/images/productos/{{$repuesto->imagenrepuesto->last()->ruta_imagenrepuesto}}" alt=""></div>
+                @endif
+                <div class="product_content">
+                    <div class="product_price number">${{  number_format($repuesto->precio_repuesto,0) }}</div>
+                    <div class="product_name"><div><a href="detallerepuesto/{{$repuesto->id_repuesto}}" tabindex="0">{{$repuesto->nombre_repuesto}}</a></div></div>
+                </div>
+                <ul class="product_marks">
+                    <li class="product_mark product_discount">-25%</li>
+                    <li class="product_mark product_new">new</li>
+                </ul>
             </div>
-            <ul class="product_marks">
-                <li class="product_mark product_discount">-25%</li>
-                <li class="product_mark product_new">new</li>
-            </ul>
-        </div>
+            @endif
         @endforeach
         
 
