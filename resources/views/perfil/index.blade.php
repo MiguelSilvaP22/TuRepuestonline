c694a78d10ce933f211fc1af08cfb7a6fe9370d8
MiguelSilvaP22 <miguel.silva22@inacapmail.cl>
2018-07-24 06:17:11 -0400

    123123

--- resources/views/perfil/index.blade.php
+++ resources/views/perfil/index.blade.php
@@ -657,7 +657,7 @@
         
                                                 @endif 
                                                 @if($venta->estado_venta==2 && $venta->nota_comprador_evaluacion==0)
-                                                <td> <button type="button" class="btn btn-primary" data-toggle="modal" data-target="#exampleModal2">Evaluar Comprador</button></td>
+                                                <td> <button type="button" class="btn btn-primary" data-toggle="modal" data-target="#exampleModal2">Evaluar Comprador</button>
         
                                                 <div class="modal fade" id="exampleModal2" tabindex="-1" role="dialog" aria-labelledby="exampleModalLabel" aria-hidden="true">
                                                     <div class="modal-dialog" role="document">
